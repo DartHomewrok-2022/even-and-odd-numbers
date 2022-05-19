f7bf2abd6b162693e1861ac3ead19e89d8867146
sanjar1218 <sanjarsaidov173@gmail.com>
2022-05-19 16:16:09 +0500

    smallchanges

--- number_odd.dart
+++ number_odd.dart
@@ -5,7 +5,7 @@
 //Print the number of even digits in the variable "var_int".
 
 void main() {
-  int var_int = 1234;
+  int var_int = 8493;
   int a = var_int % 10;
   var_int = var_int ~/ 10;
   int c = var_int % 10;
@@ -13,8 +13,8 @@ void main() {
   int b = var_int % 10;
   var_int = var_int ~/ 10;
   int d = var_int % 10;
-  print(a * ((a / 2 - a ~/ 2) * 10 - 5) +
-      b * ((b / 2 - b ~/ 2) * 10 - 5) +
-      c * ((c / 2 - c ~/ 2) * 10 - 5) +
-      d * ((d / 2 - d ~/ 2) * 10 - 5));
+  print(a * ((a / 2 - a ~/ 2) * 10 / 5) +
+      b * ((b / 2 - b ~/ 2) * 10 / 5) +
+      c * ((c / 2 - c ~/ 2) * 10 / 5) +
+      d * ((d / 2 - d ~/ 2) * 10 / 5));
 }
